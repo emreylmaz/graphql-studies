c5b337439929608cdc60634ab4b2a7c053830f38
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-04-08 21:50:29 +0300

    Update Code

--- GraphQL-HW1/index.js
+++ GraphQL-HW1/index.js
@@ -2,125 +2,132 @@ const { ApolloServer, gql } = require("apollo-server");
 const {
     ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
-const { users, events, participants, locations } = require("./data");
 
+const { locations, events, participants, users } = require("./data");
 const typeDefs = gql`
-    
     type User {
         id: ID!
         username: String!
         email: String!
-        events: [Event]!
+        events: [events!]!
+
     }
-    
-    type Event {
-        id:ID!
-        title:String!
-        desc:String!
-        date:String!
-        form:String!
-        to:String!
-        location_id:String
-        user_id:String
-        user:User
-        location:Location
-        participants: [Participant]
+    type events {
+        id: ID!
+        title: String!
+        desc: String!
+        date: String!
+        from: String!
+        to: String!
+        location_id: String!
+        user_id: String!
+        location: locations!
+        participants: [participants!]!
     }
-    
-    type Location {
+    type locations {
         id: ID!
         name: String!
         desc: String!
         lat: String!
-        Ing: String!
+        lng: String!
     }
-
-    type Participant {
+    type participants {
         id: ID!
         user_id: String!
         event_id: String!
     }
     type Query {
-        # Event
-        events: [Event!]!
-        event(id: ID!): Event!
-        
-        # User
+        # users
         users: [User!]!
-        user(id: ID!): User!
-        
-        # Location
-        locations: [Location!]!
-        location(id: ID!): Location!
-        
-        # Participant
-        participants: [Participant!]!
-        participant(id: ID!): Participant!
+        user (id: ID!): User!
+
+        # events
+        events: [events!]!
+        event(id: ID!): events!
+        # locations
+        locations: [locations!]!
+        location(id: ID!): locations
+        # participants
+        participants: [participants!]!
+        participant(id: ID!): participants
+
     }
 `;
 
 const resolvers = {
     Query: {
-        // Event
-        events: () => events,
-        event: (parent, args) => {
-            const event = events.find((event) => event.id === args.id);
-            if (!event) {
-                return new Error("Event not found");
-            }
-            return event;
-        },
-
-        // User
-        users: () => users,
-        user: (parent, args) => {
-            const user = users.find((user) => user.id === args.id);
-            if (!user) {
-                return new Error("User not found");
+        //! User Queries
+        users  : () => users,
+        user: (parent, args, context, info) => {
+            const user = users.find(user => user.id === args.id);
+            if(!user){
+                throw Error("User not found")
             }
             return user;
         },
+        //! Events Queries
+        events: () => events,
+        event : (parent, args) => {
+            const event = events.find(event => event.id === args.id);
+            if(!event){
+                throw Error("Event not found")
+            }
+            return event;
 
-        // Location
+        },
+
+        //! Locations Queries
         locations: () => locations,
         location: (parent, args) => {
-            const location = locations.find((location) => location.id === args.id);
-            if (!location) {
-                return new Error("Location not found");
+            const location = locations.find(location => location.id === args.id);
+            if(!location){
+                throw Error("Location not found")
             }
             return location;
+
         },
 
-        // Participant
+        //! Participants Queries
         participants: () => participants,
         participant: (parent, args) => {
-            const participant = participants.find(
-                (participant) => participant.id === args.id
-            );
-            if (!participant) {
-                return new Error("Participant not found");
+            const participant = participants.find(participant => participant.id === args.id);
+            if(!participant){
+                throw Error("Participant not found")
             }
             return participant;
-        },
-    },
-    Event:{
-        user:(parent)=>users.find((user)=>user.id===parent.user_id),
-        location:(parent)=>locations.find((location)=>location.id ===parent.location_id),
-        participants:(parent)=>participants.filter((participant)=>participant.event_id===parent.id)
-    },
 
-    User: {
-        events:(parent,args)=>events.filter((event)=>event.user_id===parent.id)
+        }
+
+
     },
+    User: {
+        events: (parent, args, context, info) => {
+            return events.filter(event => event.user_id === parent.id);
+        }
+    },
+    events: {
+        location: (parent, args, context, info) => {
+            return locations.find(location => location.id === parent.location_id);
+        },
+        participants: (parent, args, context, info) => {
+            return participants.filter(
+                participant => participant.event_id === parent.id
+            );
+        }
+    }
+
+
 };
 
-
-const server = new ApolloServer({ typeDefs, resolvers, plugins: [
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground({
             // options
-        })
-    ] });
-
+        }),
+    ],
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
